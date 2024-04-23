f869a5cdcc7d10b4611e2a090e661b20a402d5dd
Koozie24 <jake.mckenzie2017@gmail.com>
2024-04-23 09:49:10 -0400

    first commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+
+<style>
+    .body-title{
+        text-align: center;
+    }
+    .title-subtext{
+        text-align: center;
+    }
+</style>
+
+<html>
+
+    <head>
+        <Title>Swiss Solutions</head>
+    </head>
+
+    <body>
+        <h1 class='body-title'>Swiss Solutions</h1>
+        <p class=title-subtext'>Do you or your business have tedious and repetitive tasks that consume far too much of your time? Find out if Swiss Solutions can help you to simplify your everyday.</p>
+    </body>
+
+</html>
